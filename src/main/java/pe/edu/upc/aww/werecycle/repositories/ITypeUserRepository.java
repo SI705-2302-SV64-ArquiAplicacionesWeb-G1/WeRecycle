eedb2097ff0dbb03ca9b80e9ac5391017a3607b8
Gabriel Meza Prada <standbyg2@gmail.com>
2023-11-18 02:50:47 -0500

    Creacion de usuario permanente

--- src/main/java/pe/edu/upc/aww/werecycle/repositories/ITypeUserRepository.java
+++ src/main/java/pe/edu/upc/aww/werecycle/repositories/ITypeUserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.aww.werecycle.entities.Roles;
 
+import java.util.Optional;
+
 @Repository
 public interface ITypeUserRepository extends JpaRepository<Roles,Integer> {
 }
